2aa4822b19e5159dad51952b862b8f9967ff799b
Ruben <rubendc91@gmail.com>
2022-09-01 19:50:56 -0700

    random generator added in JS

--- Develop/script.js
+++ Develop/script.js
@@ -22,12 +22,24 @@
 
 function generatePassword(){
 //----variables
-  var lowercase = false;
-  var uppercase = false;
-  var number = false;
-  var specialCharacters = false;
-  var keylength = 0;
+  var lowercase = window.confirm("Include lowercase letters?");
+  var uppercase = window.confirm("Include uppercase letters?");
+  var number = window.confirm("Include numbers letters?");
+  var specialCharacters = window.confirm("Include special characters letters?");
+  var keylength = window.prompt("how many characters?", "8"); {  
+      // keylength = confirmLength;  
+      
+    if (keylength < 8) {
+      window.alert("Must be  more than 8.");
+      return;
+    }
+    else if(keylength > 128){
+      window.alert("Must be less than 128.");
+      return;
+    }
+  };
   
+
   console.log(lowercase);
   console.log(uppercase);
   console.log(number);
@@ -40,39 +52,41 @@ function generatePassword(){
   var numberOptions = "0123456789";
   var SpCharOptions = "!@#$%^&*()";
   var passwordOptions = "";
+  var password = "";
 //----  confirm variable values
-    var confirmLower = window.confirm("Include lowercase letters?"); 
-      if (confirmLower) {
-        lowercase = true;
-        console.log(lowercase);
-    }
-    var confirmUpper = window.confirm("Include uppercase letters?"); 
-    if (confirmUpper) {
-      uppercase = true;
-      console.log(uppercase);
-    }
-  var confirmNumber = window.confirm("Include numbers?"); 
-  if (confirmNumber) {
-    number = true;
-    console.log(number);
+  //   var confirmLower = window.confirm("Include lowercase letters?"); 
+  //     if (confirmLower) {
+  //       lowercase = true;
+  //       console.log(lowercase);
+  //   }
+  //   var confirmUpper = window.confirm("Include uppercase letters?"); 
+  //   if (confirmUpper) {
+  //     uppercase = true;
+  //     console.log(uppercase);
+  //   }
+  // var confirmNumber = window.confirm("Include numbers?"); 
+  // if (confirmNumber) {
+  //   number = true;
+  //   console.log(number);
   
-    }
-  var confirmSpChar = window.confirm("Include special characters?"); 
-  if (confirmSpChar) {
-    specialCharacters = true;
-    console.log(specialCharacters);
-    }
-  var confirmLength = window.prompt("how many characters?", "8"); {  
-    keylength = confirmLength;  
-    }  
-  if (confirmLength < 8) {
-    window.alert("Must be  more than 8.");
-    return;
-  }
-  else if(confirmLength > 128){
-    window.alert("Must be less than 128.");
-    return;
-  };
+  //   }
+  // var confirmSpChar = window.confirm("Include special characters?"); 
+  // if (confirmSpChar) {
+  //   specialCharacters = true;
+  //   console.log(specialCharacters);
+  //   }
+  // var confirmLength = window.prompt("how many characters?", "8"); {  
+  //   keylength = confirmLength;  
+  //   }  
+  // if (confirmLength < 8) {
+  //   window.alert("Must be  more than 8.");
+  //   return;
+  // }
+  // else if(confirmLength > 128){
+  //   window.alert("Must be less than 128.");
+  //   return;
+  // };
+
 //----acceppted values
  if (lowercase == true) {
    passwordOptions =  lowerOptions
@@ -88,15 +102,23 @@ function generatePassword(){
 };
 //----Characters avalible for password
 console.log(passwordOptions)
-//----Random generator
+
 for (var index = 0; index <= keylength; index++){
-  var randomNumber = Math.floor(Math.random() *keylength.length);
-   passwordOptions = (randomNumber, randomNumber +1);
+  var randomNumber = Math.floor(Math.random() *passwordOptions.length);
+ password += passwordOptions.substring(randomNumber, randomNumber +1)
+
+  console.log(keylength)
+ console.log(index)
+ 
   }
-
-console.log(index);
-
+  return password;
+// console.log(password);
+console.log(password)
 };
+//----Random generator
+
+
+// console.log(password);
 
 // random attemt 2
 
