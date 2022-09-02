bb8e33c563a57d540754dad89ddbb65a81447fb6
Ruben <rubendc91@gmail.com>
2022-09-01 21:17:12 -0700

    clean up and comment code

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,20 @@
 // Assignment code here
-
-// function passwordString(){
-  
-// }
-// console.log(keylength);
-
-//   var passwordOptions = function(){
-//     if (lowercase = true) {
-//       passwordOptions = lowerOptions
-//     }
-//     else if (uppercase = true) {
-//       passwordOptions = (passwordOptions +upperOptions )
-//     console.log (passwordOptions)
-//     }
-
-
-
-
-
-
-
 function generatePassword(){
 //----variables
   var lowercase = window.confirm("Include lowercase letters?");
   var uppercase = window.confirm("Include uppercase letters?");
   var number = window.confirm("Include numbers letters?");
   var specialCharacters = window.confirm("Include special characters letters?");
+  //alert stating you must select one option
+  while (!uppercase && !lowercase && !number && !specialCharacters) {
+    alert("You must select at least one character type!");
+    var lowercase = window.confirm("Include lowercase letters?");
+    var uppercase = window.confirm("Include uppercase letters?");
+    var number = window.confirm("Include numbers letters?");
+    var specialCharacters = window.confirm("Include special characters letters?");
+  }
   var keylength = window.prompt("how many characters?", "8"); {  
-      // keylength = confirmLength;  
-      
+//alert stating length Must be more than 8 or Must be less than 128
     if (keylength < 8) {
       window.alert("Must be  more than 8.");
       return;
@@ -39,98 +25,47 @@ function generatePassword(){
     }
   };
   
-
+//console logs variables value
   console.log(lowercase);
   console.log(uppercase);
   console.log(number);
   console.log(specialCharacters);
   console.log(keylength);
   console.log(passwordOptions);
-
+//option variables for password
   var lowerOptions =  "abcdefghijklmnopqrstuvwxyz";
   var upperOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numberOptions = "0123456789";
   var SpCharOptions = "!@#$%^&*()";
   var passwordOptions = "";
   var password = "";
-//----  confirm variable values
-  //   var confirmLower = window.confirm("Include lowercase letters?"); 
-  //     if (confirmLower) {
-  //       lowercase = true;
-  //       console.log(lowercase);
-  //   }
-  //   var confirmUpper = window.confirm("Include uppercase letters?"); 
-  //   if (confirmUpper) {
-  //     uppercase = true;
-  //     console.log(uppercase);
-  //   }
-  // var confirmNumber = window.confirm("Include numbers?"); 
-  // if (confirmNumber) {
-  //   number = true;
-  //   console.log(number);
-  
-  //   }
-  // var confirmSpChar = window.confirm("Include special characters?"); 
-  // if (confirmSpChar) {
-  //   specialCharacters = true;
-  //   console.log(specialCharacters);
-  //   }
-  // var confirmLength = window.prompt("how many characters?", "8"); {  
-  //   keylength = confirmLength;  
-  //   }  
-  // if (confirmLength < 8) {
-  //   window.alert("Must be  more than 8.");
-  //   return;
-  // }
-  // else if(confirmLength > 128){
-  //   window.alert("Must be less than 128.");
-  //   return;
-  // };
 
 //----acceppted values
  if (lowercase == true) {
-   passwordOptions =  lowerOptions
+  passwordOptions =  lowerOptions
 }
  if (uppercase == true) {
   passwordOptions = (passwordOptions + upperOptions)
  }
   if (number == true) {
-    passwordOptions = (passwordOptions + numberOptions)
+  passwordOptions = (passwordOptions + numberOptions)
   }
     if (specialCharacters == true) {
-      passwordOptions = (passwordOptions + SpCharOptions);           
+  passwordOptions = (passwordOptions + SpCharOptions)           
 };
 //----Characters avalible for password
 console.log(passwordOptions)
-
+//----generates random pass word with desiered length
 for (var index = 0; index <= keylength; index++){
   var randomNumber = Math.floor(Math.random() *passwordOptions.length);
- password += passwordOptions.substring(randomNumber, randomNumber +1)
+  password += passwordOptions.substring(randomNumber, randomNumber +1)
 
   console.log(keylength)
- console.log(index)
+  console.log(index)
  
   }
   return password;
-// console.log(password);
-console.log(password)
 };
-//----Random generator
-
-
-// console.log(password);
-
-// random attemt 2
-
-//random attempt 1
-// var index = Math.floor(Math.random() *keylength.length);
-//  generatePassword = passwordOptions[index];
-  
-
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
